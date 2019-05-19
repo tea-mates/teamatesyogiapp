b85c56bba70cd153205fc32239b34929ac2bb078
Janavi Anand <janavianand@yahoo.com>
2019-05-18 20:49:44 -0400

    cleared comment in index

--- client/index.js
+++ client/index.js
@@ -11,5 +11,5 @@ ReactDOM.render(
       <App />
     </HashRouter>
   </Provider>,
-  document.getElementById('root') // make sure this is the same as the id of the div in your index.html
+  document.getElementById('root')
 );
